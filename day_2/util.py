4e8ffbbf3d48d70b4b4c1280cb944e7a7ee8a30c
James Bellamy <j.bellamy99@gmail.com>
2024-12-28 21:17:46 +0000

    Reduce test indexes when dampening

--- day_2/util.py
+++ day_2/util.py
@@ -7,55 +7,63 @@ def parse_file(filename: str):
             reports.append([int(i) for i in str_report])
     return reports
 
+
 def is_report_safe(report, dampen_results=False):
+    safe = False
     if dampen_results:
-        result = is_report_safe_impl(report)
-        if result == True:
-            return True
+        if (result := is_report_safe_impl(report)) == True:
+            safe = True
         else:
-            # This could definaitly be improved, as we're brute force dampening rather
-            # than focusing on the failing levels 
-            for i in range(0, len(report)):
-                copied_report = report.copy()
-                copied_report.pop(i)
-                copied_result = is_report_safe_impl(copied_report)
-                if copied_result == True:
-                    return True
-            return False
+            # If damped_results is enabled, then test to see if a removing a single
+            # index makes the result safe
+            # Try removing the two indexs which caused the first attempt to fail
+            # and the first two incase it's because of the starting gradient
+            first_failing_index, second_failing_index = result
+            indexes_to_try_removing = set(
+                [first_failing_index, second_failing_index, 0, 1]
+            )
+            for index in indexes_to_try_removing:
+                if is_report_safe_without_index(report, index):
+                    safe = True
+                    break
     else:
-        result = is_report_safe_impl(report)
-        if result == True:
-            return True
-        else:
-            return False
+        if is_report_safe_impl(report) == True:
+            safe = True
+
+    print(f"Report: {report} is safe: {safe}")
+    return safe
+
+
+def is_report_safe_without_index(report, i):
+    copied_report = report.copy()
+    copied_report.pop(i)
+    copied_result = is_report_safe_impl(copied_report)
+    if copied_result == True:
+        return True
+    else:
+        return False
 
 
 def is_report_safe_impl(report):
-    print(f"Checking {report}")
     first_index = 0
     second_index = 1
     started_increasing = report[first_index] < report[second_index]
     while second_index < len(report):
-        difference  = report[first_index] - report[second_index]
+        difference = report[first_index] - report[second_index]
         abs_dif = abs(difference)
 
         if abs_dif < 1:
-            print(f"Report is changing by less than 1 because of {report[first_index]} and {report[second_index]}")
             return first_index, second_index
-        
+
         if abs_dif > 3:
-            print(f"Report is changing by more than 3 because of {report[first_index]} and {report[second_index]}")
             return first_index, second_index
-        
+
         if started_increasing:
             if difference > 0:
-                print(f"Report is no longer increasing because of {report[first_index]} and {report[second_index]}")
                 return first_index, second_index
         else:
             if difference < 0:
-                print(f"Report is no longer decreasing because of {report[first_index]} and {report[second_index]}")
                 return first_index, second_index
         first_index += 1
         second_index += 1
-    print("Report is safe")
     return True
